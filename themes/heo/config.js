786fcaebf2d0242d2f09ec319f27e43e43575208
hk6429 <87607648+hk6429@users.noreply.github.com>
2024-02-01 14:45:08 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -27,13 +27,14 @@ const CONFIG = {
   HEO_HERO_CATEGORY_3: { title: '必聽說書', url: '/tag/必聽說書' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  HEO_HERO_RECOMMEND_POST_TAG: '推荐',
+  HEO_HERO_RECOMMEND_POST_TAG: '推薦',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: true, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
   //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
     '歡迎來到好聽書齋',
+    '我們是B組：好聽書齋',
     
   ],
   HEO_INFO_CARD_URL: 'https://sound2024b.vercel.app/', // 个人资料底部按钮链接
